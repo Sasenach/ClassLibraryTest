423f64a5c38bd5ced71404528eabb6fe7d96e7e6
Sasenach <79968338+Sasenach@users.noreply.github.com>
2022-04-12 15:10:30 +0300

    Add files via upload

---
+++ Geometry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTest
+{
+    public class Geometry
+    {
+        /// <summary>
+        /// Метод подсчёта площади круга по радиусу
+        /// </summary>
+        /// <param name="radius">Радиус окружности</param>
+        /// <returns>Возвращает значение площади круга в виде числа с плавающей точкой</returns>
+        public static double Square(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        /// <summary>
+        /// Метод подсчёта площади треугольника по трём его сторонам
+        /// </summary>
+        /// <param name="a">Первая сторона</param>
+        /// <param name="b">Вторая сторона</param>
+        /// <param name="c">Третья сторона</param>
+        /// <returns>Возвращает значение площади треугольника в виде числа с плавающей точкой</returns>
+        public static double Square(double a, double b, double c)
+        {
+            double[] matrix = { a, b, c };
+            for(int i = 0; i < 3; i++)
+                for (int j = 0; j < 2; j++)
+                {
+                    if(matrix[j] > matrix[j +1])
+                    {
+                        double memory = matrix[j];
+                        matrix[j] = matrix[j + 1];
+                        matrix[j + 1] = memory;
+                    }
+                }
+            if (Math.Pow(matrix[2], 2) == Math.Pow(matrix[0], 2) + Math.Pow(matrix[1], 2)) //по теореме пифагора
+            {
+                Console.WriteLine("Фигура с данными сторонами является прямоугольным треугольником");
+                Console.ReadKey();
+            }
+            double p = (a + b + c) / 2;
+            double square = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return square;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ClassLibraryTestUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCircleSquareMethod()
+        {
+            //Arrange
+            double expected = 3.14;
+            double actual;
+            //Act
+            actual = ClassLibraryTest.Geometry.Square(1);
+            //Assert
+            Assert.AreEqual(expected, actual, 0.09);
+        }
+        [TestMethod]
+        public void TestRectangleSquareMethod()
+        {
+            //Arrange
+            double expected = 6;
+            double actual;
+            //Act
+            actual = ClassLibraryTest.Geometry.Square(3, 4, 5);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
